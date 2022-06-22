092db23e831f2f8b730825c406a718c8722c5d20
swvmpdad <will.g.camp@gmail.com>
2022-06-21 23:20:54 -0500

    Added array with questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+var questions = [
+    {
+        prompt: 1,
+        question: "When storing data in local storage, in what format does Javascript save it?",
+        answer: "String",
+        options: [
+            "Array",
+            "String",
+            "Boolean",
+            "Integer"
+        ]
+    },
+    {
+        prompt: 2,
+        question: "What is the proper html tag to add a javascript reference file in your code?",
+        answer: "<script>",
+        options: [
+            "<link>",
+            "<a href>",
+            "<meta>",
+            "<script>"
+        ]
+    },
+    {
+        prompt: 3,
+        question: "What type of variable is needed to store multiple items in a single variable?",
+        answer: "Array",
+        options: [
+            "Array",
+            "Boolean",
+            "String",
+            "Integer"
+        ]
+    },
+    {
+        prompt: 4,
+        question: "What code is needed to display a message in the console of your developer tools?",
+        answer: "console.log()",
+        options: [
+            "window.alert()",
+            "window.prompt()",
+            "console.log()",
+            "alert()"
+        ]
+    }
+];
+
+
